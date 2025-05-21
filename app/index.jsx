3a5b4452d961cd6d97838a8b50186ae8abd4fa1a
jakebogan01 <jakebogan01@gmail.com>
2025-05-21 17:55:23 -0400

    adds basic styles on home

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
+
 const Home = () => {
 	return (
-		<View>
-			<Text>Home</Text>
+		<View style={styles.container}>
+			<Image source={Logo} style={styles.image} />
+			<Text style={[styles.title, {color: 'purple'}]}>Home</Text>
+			<Text style={{marginTop: 10, marginBottom: 30}}>Another line</Text>
 		</View>
 	)
 }
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	image: {
+		marginVertical: 20
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 18
+	}
+})
