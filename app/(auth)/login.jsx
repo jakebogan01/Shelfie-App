3c0a3363e24b77e98995bf1757da23f892e5cf55
jakebogan01 <jakebogan01@gmail.com>
2025-05-23 20:02:35 -0400

    adds themed button component

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,11 +1,17 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
+import ThemedButton from '../../components/ThemedButton'
 import Spacer from '../../components/Spacer'
 
 const Login = () => {
+
+	const handleSubmit = async () => {
+		console.log('login form submitted')
+	}
+
 	return (
 		<ThemedView style={styles.container}>
 
@@ -13,6 +19,10 @@ const Login = () => {
 			<ThemedText title={true} style={styles.title}>
 				Login to Your Account
 			</ThemedText>
+
+			<ThemedButton onPress={handleSubmit}>
+				<Text style={{ color: '#f2f2f2' }}>Login</Text>
+			</ThemedButton>
 
 			<Spacer height={100} />
 			<Link href="/register" replace>
@@ -31,6 +41,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: "center",
+		alignItems: "center"
 	},
 	title: {
 		textAlign: "center",
